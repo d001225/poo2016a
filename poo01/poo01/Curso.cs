abd9a827959219e803234a08a4d09b65048e6505
Henrique Silva <d001225@ismai.pt>
2017-03-07 13:33:38 +0000

    Resultado no final da Aula de 24 de fevereiro Primeiras classes criadas Infraestrutura da Solução

---
+++ poo01/poo01/Curso.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace poo01 {
+   public class Curso {
+      public string Nome { get; set; }
+
+      private byte nAnos;
+      public byte NumAnos {
+         get { return nAnos; }
+         set {
+            int[] anosPossiveis = AnosPossiveisTipoDeCurso();
+            bool valorValido = false;
+            foreach (var v in anosPossiveis) {
+               if (value == v) valorValido = true;
+            }
+            if (!valorValido) { throw new Exception(); }
+            nAnos = value;
+         }
+      }
+
+      private int[] AnosPossiveisTipoDeCurso() {
+         switch (Tipo) {
+            case TipoCurso.CTESP: return new[] { 2 }; 
+            case TipoCurso.Licenciatura: return new[] { 3, 4 }; 
+            case TipoCurso.MestradoIntegrado:return new[] { 5, 6, 7 };
+            case TipoCurso.Mestrado:return new[] { 2, 3 };
+            case TipoCurso.Doutoramento:return new[] { 3 };
+            default:return new int[0];
+         }
+      }
+
+      public TipoCurso Tipo { get; set; }
+   }
+
+   public enum TipoCurso {
+      CTESP, Licenciatura, MestradoIntegrado,
+      Mestrado, Doutoramento
+   }
+}
